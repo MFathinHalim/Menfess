79e5e0c842aba9e10f158355182894b1116021c0
M-Fathin-Halim <99858315+M-Fathin-Halim@users.noreply.github.com>
2023-01-23 10:28:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post('/delete', (req, res) => {
 	})
 })
 
-app.listen(3000, (req, res) => {
+app.listen(8000, (req, res) => {
 	Host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
 	console.log("App is running on port 3000")
 })

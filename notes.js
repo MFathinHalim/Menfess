7d106defc8d49997dc769201202e77d8825ac84f
M-Fathin-Halim <99858315+M-Fathin-Halim@users.noreply.github.com>
2023-01-26 14:16:38 +0700

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,18 @@
+notes = [{
+    noteId: 1,
+    noteContent: "Hai.",
+    noteName: "Doma"
+},
+{
+    noteId: 2,
+    noteContent: "Selamat Datang di Menfess! Ungkapin Aja!",
+    noteName: "Doma"
+},
+{
+    noteId: 3,
+    noteContent: "Aku cinta Mikel banyak-banyak",
+    noteName: "Bang Leyas, Mikelll"
+}
+]
+
+module.exports = {notes}

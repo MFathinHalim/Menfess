225e647dcbc0d8fcf78c6728e1c7bae9e18e3888
M-Fathin-Halim <99858315+M-Fathin-Halim@users.noreply.github.com>
2023-01-26 16:17:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const path = require('path');
 const express = require('express')
 const bodyParser = require('body-parser')
+const sqlite3 = require('sqlite3').verbose();
+let sql;
+
+const db = new sqlite3.Database("./test.db", sqlite3.OPEN_READWRITE, (err) => {
+	if (err) return console.error(err.message);
+});
+//Create table
+//sql = 'CREATE TABLE data(id INTEGER PRIMARY KEY,noteId,noteContent,noteName)';
+//db.run(sql);
+
 const {notes} = require("./notes")
 
+
+var data = [];
 const app = express()
+sql = 'SELECT * FROM data';
+		db.all(sql, [], (err, rows) => {
+			if(err) return console.error(err.message);
+			rows.forEach((row) => {
+				console.log(row);
+				data.push({
+					noteId:row.noteId,
+					noteContent: row.noteContent,
+					noteName: row.noteName
+				});
+				
+			});
+});
 
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '/public')));
@@ -14,8 +39,21 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.get("/", function (req, res) {
+	sql = 'SELECT * FROM data';
+		db.all(sql, [], (err, rows) => {
+			if(err) return console.error(err.message);
+			rows.forEach((row) => {
+				console.log(row);
+				data.push({
+					noteId:row.noteId,
+					noteContent: row.noteContent,
+					noteName: row.noteName
+				});
+				
+			});
+});
 	res.render("home", {
-		data: notes
+		data: data
 	})
 })
 
@@ -27,76 +65,69 @@ app.post("/", (req, res) => {
 	
     // All good
 	if(noteName!="" && noteContent!="" && noteName.toLowerCase()!="test" && noteContent.toLowerCase()!="test"){
-		notes.push({
-			noteId: noteId,
-			noteContent: noteContent,
-			noteName: noteName,
-		})
-	}
-	
-	res.render("home", {
-		data: notes
-	})
-})
-
-app.post('/comment', (req, res) => {
-	const noteContent = req.body.noteContentComment
-    const noteName = req.body.noteNameComment
-	const noteId = req.body.noteId;
-	const noteIdComment = noteId + "y";
-	
-    // All good
-	if(noteName!="" && noteContent!=""){
-		notes.push({
-			noteId: noteId,
-			noteIdComment: noteIdComment,
-			noteContentComment: noteContent,
-			noteNameComment: noteName,
+		//notes.push({
+			//noteId: noteId,
+			//noteContent: noteContent,
+			//noteName: noteName,
 			
-		})
-	}
-	
-	res.render("home", {
-		data: notes
-	})
-})
-
-
-app.post('/update', (req, res) => {
-	var noteId = req.body.noteId;
-	var noteContent = req.body.noteContent;
-    var noteName = req.body.noteName;
-	var noteIdComment = req.body.noteIdComment;
-	var noteContentComment= req.body.noteContentComment;
-    var noteNameComment = req.body.noteNameComment;
-	console.log(noteId);
-	
-	
-	notes.forEach(note => {
-		if (note.noteId == noteId) {
-			if(noteName!="" && noteContent!=""){
-				note.noteContent = noteContent;
-				note.noteName = noteName;
-
-			}
-		}
-		if (note.noteIdComment == noteIdComment) {
-			if(noteNameComment!="" && noteContentComment!=""){
-				note.noteContentComment = noteContentComment;
-				note.noteNameComment = noteNameComment;
-			}
+			//})
 			
+			sql = 'INSERT INTO data(noteId,noteContent,noteName) VALUES (?,?,?)';
+			db.run(sql, [noteId, noteContent, noteName], (err) => {
+				if (err) return console.error(err.message);
+			})
 		}
 		
+		sql = 'SELECT * FROM data';
+		db.all(sql, [], (err, rows) => {
+			if(err) return console.error(err.message);
+			rows.forEach((row) => {
+				console.log(row);
+				data.push({
+					noteId:row.noteId,
+					noteContent: row.noteContent,
+					noteName: row.noteName
+				});
+				
+			});
+		});
+		res.render("home", {
+			data: data
+		})
 	})
-	res.render("home", {
-		data: notes
+	
+	app.post('/update', (req, res) => {
+		var noteId = req.body.noteId;
+		var noteContent = req.body.noteContent;
+		var noteName = req.body.noteName;
+		var noteIdComment = req.body.noteIdComment;
+		var noteContentComment= req.body.noteContentComment;
+		var noteNameComment = req.body.noteNameComment;
+		console.log(noteId);
+		
+		
+		sql = 'SELECT * FROM data';
+		db.all(sql, [], (err, rows) => {
+			if(err) return console.error(err.message);
+			rows.forEach((row) => {
+				console.log(row);
+				data.push({
+					noteId:row.noteId,
+					noteContent: row.noteContent,
+					noteName: row.noteName
+				});
+				
+			});
+		});
+
+		res.render("home", {
+			data: data
+		})
 	})
-})
 
 app.post('/delete', (req, res) => {
 	var noteId = req.body.noteId;
-
+	
 	var j = 0;
 	notes.forEach(note => {
 		j = j + 1;
@@ -104,31 +135,41 @@ app.post('/delete', (req, res) => {
 			notes.splice((j - 1), 1)
 		}
 	})
-
+	sql = 'SELECT * FROM data';
+		db.all(sql, [], (err, rows) => {
+			if(err) return console.error(err.message);
+			rows.forEach((row) => {
+				console.log(row);
+				data.push({
+					noteId:row.noteId,
+					noteContent: row.noteContent,
+					noteName: row.noteName
+				});
+				
+			});
+});
 	res.render("home", {
-		data: notes
+		data: data
 	})
 })
-app.post('/deleteCom', (req, res) => {
-	var noteId = req.body.noteIdComment;
 
-	var j = 0;
-	notes.forEach(note => {
-		j = j + 1;
-		if (note.noteId == noteId) {
-			notes.splice((j - 1), 1)
-		}
-	})
-
-	res.render("home", {
-		data: notes
-	})
-})
 
 app.post('/r', (req, res) => {
-
+	sql = 'SELECT * FROM data';
+		db.all(sql, [], (err, rows) => {
+			if(err) return console.error(err.message);
+			rows.forEach((row) => {
+				console.log(row);
+				data.push({
+					noteId:row.noteId,
+					noteContent: row.noteContent,
+					noteName: row.noteName
+				});
+				
+			});
+});
 	res.render("home", {
-		data: notes
+		data: data
 	})
 })
 

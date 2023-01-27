7b4b4443e71103cbfcc2c0f8d946e6e0f0add8b9
M-Fathin-Halim <99858315+M-Fathin-Halim@users.noreply.github.com>
2023-01-27 13:32:53 +0700

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,18 +1,3 @@
-notes = [{
-    noteId: 1,
-    noteContent: "Hai.",
-    noteName: "Doma"
-},
-{
-    noteId: 2,
-    noteContent: "Selamat Datang di Menfess! Ungkapin Aja!",
-    noteName: "Doma"
-},
-{
-    noteId: 3,
-    noteContent: "Aku cinta Mikel banyak-banyak",
-    noteName: "Bang Leyas, Mikelll"
-}
-]
+notes = []
 
 module.exports = { notes }

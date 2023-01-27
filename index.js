7a1958dfe501e0061b468aa707d545dbdff6bcac
M-Fathin-Halim <99858315+M-Fathin-Halim@users.noreply.github.com>
2023-01-27 13:16:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ app2.listen(3000, (req, res) => {
 	Host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
 	console.log("App is running on port 3000")
 })
+app1.listen(3000, (req, res) => {
+	Host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+	console.log("App is running on port 3000")
+})

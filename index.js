ff42d42742f8c7b46dfad2899b54ee98dd804644
M-Fathin-Halim <99858315+M-Fathin-Halim@users.noreply.github.com>
2023-01-27 13:15:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var app1 = require('./app.js').app
 var app2 = require('./admin.js').app
 
-app1.listen(3000, (req, res) => {
+app2.listen(3000, (req, res) => {
 	Host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
 	console.log("App is running on port 3000")
-})
-
-app2.listen(3030, (req, res) => {
-	Host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-	console.log("Admin is running on port 3030")
 })

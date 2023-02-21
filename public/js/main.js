d414dec651cd238de22d53db71d642520e7f412d
M-Fathin-Halim <99858315+M-Fathin-Halim@users.noreply.github.com>
2023-02-21 17:13:31 +0700

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+function removeElement(element) {
+    element.remove();
+}
+function myFunction() {
+    var x = document.getElementById("background");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+let mybutton = document.getElementById("reload");
+
+function myFunction2() {
+    var x = document.getElementById("comment");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}

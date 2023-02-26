fa4d691a545a300571a3a81636b9b4e973250409
M-Fathin-Halim <99858315+M-Fathin-Halim@users.noreply.github.com>
2023-02-26 11:41:52 +0700

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,32 @@
+
+
+window.onload = function() {
+
+    var pageTitle = document.title;
+    var attentionMessage = '(' + Math.floor(Math.random() * 10).toString() + ') Ada yang confess!';
+    var blinkEvent = null;
+  
+    document.addEventListener('visibilitychange', function(e) {
+      var isPageActive = !document.hidden;
+  
+      if(!isPageActive){
+        blink();
+      }else {
+        document.title = pageTitle;
+        clearInterval(blinkEvent);
+      }
+    });
+  
+    function blink(){
+      blinkEvent = setInterval(function() {
+        if(document.title === attentionMessage){
+          document.title = pageTitle;
+        }else {
+          document.title = attentionMessage;
+        }
+      }, 100);
+    }
+  };
 function removeElement(element) {
     element.remove();
 }
